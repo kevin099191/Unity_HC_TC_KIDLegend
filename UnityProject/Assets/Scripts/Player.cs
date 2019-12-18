4bb593c2341350a3f8a85f9344c0ccd6b5a773dc
kevin099191 <57181260+kevin099191@users.noreply.github.com>
2019-12-18 21:30:15 +0800

    搖桿操作

---
+++ UnityProject/Assets/Scripts/Player.cs
@@ -0,0 +1,57 @@
+﻿
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Joystick joystick;
+    private Rigidbody rig;
+    public float speed=25;
+
+
+
+
+
+
+    private void Move()
+    {
+        float v = joystick.Vertical;
+        float h = joystick.Horizontal;
+
+
+
+
+
+
+        rig.AddForce(h * speed, 0, v * speed);        
+
+
+
+
+    }
+
+
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+
+    private void Start()
+    {
+
+
+        rig = GetComponent<Rigidbody>();
+        joystick = GameObject.Find("虛擬搖桿").GetComponent<Joystick>();
+
+
+
+
+
+
+    }
+
+
+
+
+}
