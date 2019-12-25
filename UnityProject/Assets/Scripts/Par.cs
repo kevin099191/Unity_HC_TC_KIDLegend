ebafa6cc32acdfdf4e6c1e110dd7483e2973acf9
kevin099191 <57181260+kevin099191@users.noreply.github.com>
2019-12-25 21:35:14 +0800

    捲動技能

---
+++ UnityProject/Assets/Scripts/Par.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class Par : MonoBehaviour
+{
+
+    public Transform[] ts;
+
+    public GameObject[] fa = new GameObject[5];
+
+    public string[] s = new string[4];
+
+    public GameObject[] player;
+
+
+    private void Start()
+    {
+       
+
+
+
+        int Ca = 100;
+
+        while (Ca > 60)
+        {
+            Ca-=10;
+            print("whilec迴圈次數:"+ Ca);
+        }
+
+
+        player = GameObject.FindGameObjectsWithTag("Player");
+
+
+
+
+    }
+
+
+
+
+
+}
---
+++ UnityProject/Assets/Scripts/RandomSkill.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+
+
+
+public class RandomSkill : MonoBehaviour
+{
+    public Sprite[] SpritesBlur;
+    public Sprite[] SpritesSkill;
+
+    public Image imageSkill;
+
+    public float speed=0.1f;
+
+
+    private IEnumerator RandomEffect()
+    {
+        for (int i = 0; i < SpritesBlur.Length; i++)
+        {
+            imageSkill.sprite = SpritesBlur[i];
+            yield return new WaitForSeconds(speed);
+
+
+        }
+
+
+    }
+
+
+
+
+    private void Start()
+    {
+        StartCoroutine(RandomEffect());
+    }
+
+
+
+}
