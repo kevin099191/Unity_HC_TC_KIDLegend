768a15186d87b7696b5c6db1c113b70ac2d881db
kevin099191 <57181260+kevin099191@users.noreply.github.com>
2019-12-27 20:50:27 +0800

    技能按鈕完成

--- UnityProject/Assets/Scripts/RandomSkill.cs
+++ UnityProject/Assets/Scripts/RandomSkill.cs
@@ -9,23 +9,55 @@ public class RandomSkill : MonoBehaviour
 {
     public Sprite[] SpritesBlur;
     public Sprite[] SpritesSkill;
+    private string[] NamesSkill = {"連續射擊","正向箭","背向箭","側向箭","血量增加","攻擊增加","攻速增加","爆擊增加",};
+    
 
-    public Image imageSkill;
+    public float speed=0.02f;
+    [Header("捲動次數"),Range(1,30)]
+    public int counts=5;
 
-    public float speed=0.1f;
+    private AudioSource aud;
+    public AudioClip sound;
+    public AudioClip soundskill;
+
+    private Image imageSkill;
+    private Button btn;
+    private Text Textname;
+    private GameObject skillsplane;
+    private int index;
+
+
+
 
 
     private IEnumerator RandomEffect()
     {
-        for (int i = 0; i < SpritesBlur.Length; i++)
+        btn.interactable = false;
+        for (int j = 0; j < counts; j++)
         {
-            imageSkill.sprite = SpritesBlur[i];
-            yield return new WaitForSeconds(speed);
+
+            
+
+            for (int i = 0; i < SpritesBlur.Length; i++)
+            {
+                aud.PlayOneShot(sound, 0.5f);
+                imageSkill.sprite = SpritesBlur[i];
+                yield return new WaitForSeconds(speed);
 
 
+            }
         }
 
 
+        btn.interactable = true;
+
+        index = Random.Range(0, SpritesSkill.Length);
+        imageSkill.sprite = SpritesSkill[index];
+        aud.PlayOneShot(soundskill, 0.5f);
+
+        Textname.text = NamesSkill[index];
+
+        
     }
 
 
@@ -33,8 +65,32 @@ public class RandomSkill : MonoBehaviour
 
     private void Start()
     {
+        aud = gameObject.GetComponent<AudioSource>();
+        imageSkill = GetComponent<Image>();
+        btn = GetComponent<Button>();
+        Textname = transform.GetChild(0).GetComponent<Text>();
+        skillsplane = GameObject.Find("隨機");
+        btn.onClick.AddListener(choeseskill);
+
+
+
+
+
         StartCoroutine(RandomEffect());
+        
+
     }
+
+
+    private void choeseskill()
+    {
+        skillsplane.SetActive(false);
+        print(NamesSkill[index]);
+
+    }
+
+
+
 
 
 
